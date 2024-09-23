7a4410b5b016efaa5f14141d73970c54df91e547
Punxmunx <38075219+Punxmunx@users.noreply.github.com>
2024-09-23 18:58:46 -0400

    Update hellowhatday.py

--- hellowhatday.py
+++ hellowhatday.py
@@ -2,5 +2,5 @@
 def main()
 name = input("what is your name, User?:")
 user_input = input("what day of the week is it, User?:")
-Print("Hello,:", Name, "Happy:", input1:)
+Print("Hello,:", Name, "Happy:", input:)
 main()
