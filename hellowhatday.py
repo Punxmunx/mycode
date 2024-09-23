2eb734b11f7ee2e0588b58526abad7a19f31ea6d
punxmunx <jonathanismoore@proton.me>
2024-09-23 22:57:16 +0000

    hellowhatday.py

---
+++ hellowhatday.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python#
+def main()
+name = input("what is your name, User?:")
+user_input = input("what day of the week is it, User?:")
+Print("Hello,:", Name, "Happy:", input1:)
+main()
