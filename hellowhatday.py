802916d6f718df579a7298ce342be54891a9d903
punxmunx <jonathanismoore@proton.me>
2024-10-01 04:05:51 +0000

    code repair

--- hellowhatday.py
+++ hellowhatday.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python#
-def main()
-name = input("what is your name, User?:")
-user_input = input("what day of the week is it, User?:")
-Print("Hello,:", Name, "Happy:", input:)
+#!/usr/bin/env python3
+def main():
+    name = input("what is your name, User?")
+    user_input = input("what day of the week is it, User?")
+    print("Hello,",name, "Happy", user_input)
 main()
