5d171f24a9c21406d4c42d4891c37392a8068775
punxmunx <jonathanismoore@proton.me>
2024-10-04 02:13:51 +0000

    playing with python pip WITH GLOVES

---
+++ if_name/first_module.py
@@ -0,0 +1,15 @@
+#def main():
+#    print("Module #1 Name=", __name__)
+#if __name__=="__main__":
+ #   print("code is python")
+ #   main()
+#else:
+ #   print("code is imported")
+print("This code will always execute.")
+
+def main():
+   print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+
+    main()
